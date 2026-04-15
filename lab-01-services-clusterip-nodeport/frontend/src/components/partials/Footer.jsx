e34c3bee905bc1ac9ec8c0541af93bd496bf04da
keamysh <keamysh@gmail.com>
2026-04-14 20:07:55 -0600

    Add Footer component to the project

---
+++ lab-01-services-clusterip-nodeport/frontend/src/components/partials/Footer.jsx
@@ -0,0 +1,14 @@
+import { Col, Container, Row } from "react-bootstrap";
+export default function Footer(){
+    return(
+        <footer>
+            <Container className="h-100">
+                <Row className="h-100">
+                    <Col md={4}></Col>
+          
+                    <Col md={4}></Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
